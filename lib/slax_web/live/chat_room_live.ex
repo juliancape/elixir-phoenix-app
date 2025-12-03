42a372861de128e49e1ca38434b4ef3bd7ef7dc3
juliancape <juliancape360@gmail.com>
2025-12-03 16:57:33 -0500

    remove header from layout

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -3,7 +3,10 @@ defmodule SlaxWeb.ChatRoomLive do
 
   def render(assigns) do
     ~H"""
-    <div>Welcome to the chat!</div>
+    <div>Welcome to the chat! {2 + 4} or <%= 2 + 6 %> or <%!-- 2+4 --%> </div>
+    <Layouts.app flash={@flash}>
+      <div> Welcome to the chat!</div>
+    </Layouts.app>
     """
   end
 end
